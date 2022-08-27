9f0f6c2620a37c756a957b2fac52ec4d51f80078
bazla <dobr1nya@mail.ru>
2022-08-27 11:18:16 +0300

    feat: create about-us

--- src/container/AboutUs/AboutUs.tsx
+++ src/container/AboutUs/AboutUs.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import './AboutUs.scss';
+import G from '../../assets/G.png';
+import spoon from '../../assets/spoon.svg';
+import knife from '../../assets/knife.png';
 
-import './AboutUs.css';
-
-const AboutUs = () => <div>AboutUs</div>;
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={G} alt="bg G" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About us</h1>
+        <img src={spoon} alt="spoon" className="spoon__image" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className="custom__button">Know More</button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={knife} alt="knife" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={spoon} alt="spoon" className="spoon__image" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className="custom__button">Know More</button>
+      </div>
+    </div>
+  </div>
+);
 
 export default AboutUs;
